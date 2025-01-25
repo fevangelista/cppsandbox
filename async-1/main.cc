3ceda5f17bb7c412dde2d85eba64bb2678f63c3b
fevangelista <francesco.evangelista@emory.edu>
2025-01-24 23:27:54 -0500

    Add async example

---
+++ async-1/main.cc
@@ -0,0 +1,25 @@
+#include <chrono>
+#include <future>
+#include <iostream>
+#include <string>
+
+// do some work in a background thread
+std::string do_work() {
+  using namespace std::chrono_literals;
+  std::this_thread::sleep_for(1s); // Simulate a time-consuming work
+  return "Hello from std::async!";
+}
+
+int main() {
+  // launch do_work() in a background thread and store the future result
+  auto future_result = std::async(std::launch::async, do_work);
+
+  // do some other work in the main thread
+  std::cout << "Main thread continue execution while do_work() runs...\n";
+
+  // wait for do_work() to finish, then retrieve the result
+  auto result = future_result.get();
+  std::cout << "Result from background thread: " << result << "\n";
+
+  return 0;
+}
